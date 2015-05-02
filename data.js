812ae722a3facc875ee4116da42dfeefb762897c
Ryan Russell <russellsprouts2@gmail.com>
2015-05-02 16:45:07 -0700

    Clean up, auto re-centering

--- data.js
+++ data.js
@@ -1,14 +1,16 @@
 sun = {
   r: 695500,
   "color": "yellow",
+  "name": "Sun (Sol)",
   "satellites": [
-    {"name":"Mercury", "ap": 69816900, "r": 2439.7, "color": "#999", "satellites":[]},
-    {"name":"Venus", "ap": 108939000, "r": 6051.8, "color": "#EA0", "satellites":[]},
+    {"name":"Mercury", "ap": 69816900, "r": 2439.7, "color": "#999", "period": 87.969, "satellites":[]},
+    {"name":"Venus", "ap": 108939000, "r": 6051.8, "color": "#EA0", "period": 224.701, "satellites":[]},
     {
       "name":"Earth",
       "ap": 151930000,
       "r": 6371.0,
       "color": "#03F",
+      "period": 365.256363004,
       "satellites": [
         {"name":"Moon (Luna)", "ap":405400, "r": 1737.10, "color":"#999", "period":27.321582}
       ]
@@ -18,6 +20,7 @@ sun = {
       "ap": 249.2e6,
       "r": 3389.5,
       "color": "#F00",
+      "period": 686.971,
       "satellites": [
         {"name": "Phobos", "r":11.1, "ap":9377, "color":"#B66", "period":0.31891023},
         {"name": "Diemos", "r":6.2, "ap":23460, "color":"#B66", "period":1.263}
@@ -28,6 +31,7 @@ sun = {
       "ap": 816520800,
       "r": 69911,
       "color": "#FA1",
+      "period": 4332.52245,
       "satellites": [
         {
           "name": "Io",
@@ -93,54 +97,55 @@ sun = {
       "r": 58232,
       "color": "#FD1",
       "rings": true,
+      "period": 10759.22,
       "satellites": [
         {
           "name": "Mimas",
-          "r": "198",
-          "ap": "185539",
-          "period": "0.942422",
+          "r": 198,
+          "ap":185539,
+          "period":0.942422,
           "color": "gray"
         },
         {
           "name": "Enceladus",
-          "r": "252",
-          "ap": "237948",
-          "period": "1.370218",
+          "r": 252,
+          "ap":237948,
+          "period":1.370218,
           "color": "#77F"
         },
         {
           "name": "Tethys",
-          "r": "531",
-          "ap": "294619",
-          "period": "1.887802",
+          "r": 531,
+          "ap":294619,
+          "period":1.887802,
           "color": "gray"
         },
         {
           "name": "Dione",
-          "r": "561.5",
-          "ap": "377396",
-          "period": "2.736915",
+          "r": 561.5,
+          "ap":377396,
+          "period":2.736915,
           "color": "gray"
         },
         {
           "name": "Rhea",
-          "r": "763.5",
-          "ap": "527108",
-          "period": "4.518212",
+          "r": 763.5,
+          "ap":527108,
+          "period":4.518212,
           "color": "gray"
         },
         {
           "name": "Titan",
-          "r": "2575",
-          "ap": "1221870",
-          "period": "15.94542",
+          "r": 2575,
+          "ap":1221870,
+          "period":15.94542,
           "color": "#d90"
         },
         {
           "name": "Iapetus",
-          "r": "735",
-          "ap": "3560820",
-          "period": "79.3215",
+          "r": 735,
+          "ap":3560820,
+          "period":79.3215,
           "color": "#333"
         }
       ]
@@ -150,53 +155,48 @@ sun = {
       "ap": 3006224700,
       "r": 25362,
       "color": "#0FF",
+      "period": 30687.15,
       "satellites": [
         {
-          "name": "Titania",
-          "diameter": "1576.8",
-          "r": "788.4",
-          "ap": "435910",
-          "period": "8.705872",
+          "name": "Titania",  
+          "r": 788.4,
+          "ap":435910,
+          "period":8.705872,
           "color": "gray"
         },
         {
           "name": "Oberon",
-          "diameter": "1522.8",
-          "r": "761.4",
-          "ap": "583520",
-          "period": "13.463239",
+          "r": 761.4,
+          "ap":583520,
+          "period":13.463239,
           "color": "gray"
         },
         {
           "name": "Ariel",
-          "diameter": "1157.8",
-          "r": "578.9",
-          "ap": "191020",
-          "period": "2.520379",
+          "r": 578.9,
+          "ap":191020,
+          "period":2.520379,
           "color": "gray"
         },
         {
           "name": "Umbriel",
-          "diameter": "1169.4",
-          "r": "584.7",
-          "ap": "266300",
-          "period": "4.144177",
+          "r": 584.7,
+          "ap":266300,
+          "period":4.144177,
           "color": "#444"
         },
         {
           "name": "Miranda",
-          "diameter": "471.6",
-          "r": "235.8",
-          "ap": "129390",
-          "period": "1.413479",
+          "r": 235.8,
+          "ap":129390,
+          "period":1.413479,
           "color": "#999"
         },
         {
           "name": "Puck",
-          "diameter": "162",
-          "r": "81",
-          "ap": "86010",
-          "period": "0.761833",
+          "r": 81,
+          "ap":86010,
+          "period":0.761833,
           "color": "gray"
         }
       ]        
@@ -206,55 +206,56 @@ sun = {
       "ap": 4537580900,
       "r": 24622,
       "color": "#00F",
+      "period": 60190.03,
       "satellites": [
         {
           "name": "Naiad",
-          "diameter": "66",
-          "r": "33",
-          "ap": "48227",
-          "period": "0.294"
+          "r": 33,
+          "ap":48227,
+          "period": "0.294",
+          "color": "grey"
         },
         {
           "name": "Thalassa",
-          "diameter": "82",
-          "r": "41",
-          "ap": "50074",
-          "period": "0.311"
+          "r": 41,
+          "ap":50074,
+          "period": "0.311",
+          "color": "grey"
         },
         {
           "name": "Despina",
-          "diameter": "150",
-          "r": "75",
-          "ap": "52526",
-          "period": "0.335"
+          "r": 75,
+          "ap":52526,
+          "period": "0.335",
+          "color": "grey"
         },
         {
           "name": "Galatea",
-          "diameter": "176",
-          "r": "88",
-          "ap": "61953",
-          "period": "0.429"
+          "r": 88,
+          "ap":61953,
+          "period": "0.429",
+          "color": "grey"
         },
         {
           "name": "Larissa",
-          "diameter": "194",
-          "r": "97",
-          "ap": "73548",
-          "period": "0.555"
+          "r": 97,
+          "ap":73548,
+          "period": "0.555",
+          "color": "grey"
         },
         {
           "name": "Proteus",
-          "diameter": "420",
-          "r": "210",
-          "ap": "117646",
-          "period": "1.122"
+          "r": 210,
+          "ap":117646,
+          "period": "1.122",
+          "color": "grey"
         },
         {
           "name": "Triton",
-          "diameter": "2705.2",
-          "r": "1352.6",
-          "ap": "354759",
-          "period": "−5.877"
+          "r": 1352.6,
+          "ap":354759,
+          "period": -5.877,
+          "color": "#699"
         }
       ]
     }
---
+++ sound.js
@@ -0,0 +1,28 @@
+var notes = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];    
+var note = function(freq) {
+  return notes[(Math.round(12*(Math.log(freq/440) / Math.log(2))))%12];
+}
+
+console.log(note(1232));
+
+var synth = T("OscGen", {wave:"sin", mul:0.5});
+
+T("interval", {interval: 300, timeout:"5sec"}, function() {
+    synth.noteOnWithFreq(400, 80);
+}).on("ended", function() {
+    this.stop();
+}).set({buddies:synth}).start();
+
+T("interval", {interval: 400, timeout:"5sec"}, function() {
+    synth.noteOnWithFreq(300, 80);
+}).on("ended", function() {
+    this.stop();
+}).set({buddies:synth}).start();
+
+
+T("interval", {interval: 100, timeout:"5sec"}, function() {
+    synth.noteOnWithFreq(800, 80);
+}).on("ended", function() {
+    this.stop();
+}).set({buddies:synth}).start();
+
