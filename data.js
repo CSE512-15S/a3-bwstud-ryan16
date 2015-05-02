0fe11bd2858e56c397a7c6998ae9963d031afb8d
Ryan Russell <russellsprouts2@gmail.com>
2015-05-02 14:53:10 -0700

    Moved data to new file

---
+++ data.js
@@ -0,0 +1,262 @@
+sun = {
+  r: 695500,
+  "color": "yellow",
+  "satellites": [
+    {"name":"Mercury", "ap": 69816900, "r": 2439.7, "color": "#999", "satellites":[]},
+    {"name":"Venus", "ap": 108939000, "r": 6051.8, "color": "#EA0", "satellites":[]},
+    {
+      "name":"Earth",
+      "ap": 151930000,
+      "r": 6371.0,
+      "color": "#03F",
+      "satellites": [
+        {"name":"Moon (Luna)", "ap":405400, "r": 1737.10, "color":"#999", "period":27.321582}
+      ]
+    },
+    {
+      "name":"Mars",
+      "ap": 249.2e6,
+      "r": 3389.5,
+      "color": "#F00",
+      "satellites": [
+        {"name": "Phobos", "r":11.1, "ap":9377, "color":"#B66", "period":0.31891023},
+        {"name": "Diemos", "r":6.2, "ap":23460, "color":"#B66", "period":1.263}
+      ]
+    },
+    {
+      "name":"Jupiter",
+      "ap": 816520800,
+      "r": 69911,
+      "color": "#FA1",
+      "satellites": [
+        {
+          "name": "Io",
+          "r": 1821.6,
+          "ap":421700,
+          "color":"#fc0",
+          "period":1.7691
+        },
+        {
+          "name": "Europa",
+          "r": 1560.8,
+          "ap": 671034,
+          "color": "#f60",
+          "period": 3.5512
+        },
+        {
+          "name": "Ganymede",
+          "r": 2631.2,
+          "ap": 1070412,
+          "color": "gray",
+          "period": 7.1546
+        },
+        {
+          "name": "Callisto",
+          "r": 2410.3,
+          "ap": 1882709,
+          "color": "#A6A",
+          "period": 16.689
+        },
+        {
+          "name": "Amalthea",
+          "r": 83.45,
+          "ap": 181366,
+          "color": "gray",
+          "period": 0.49817943
+        },
+        {
+          "name": "Thebe",
+          "r": 49.3,
+          "ap": 221889,
+          "color": "gray",
+          "period": 0.674536
+        },
+        {
+          "name": "Adrastea",
+          "r": 8.2,
+          "ap": 128690,
+          "color": "gray",
+          "period": 0.29826
+        },
+        {
+          "name": "Metis",
+          "r": 21.5,
+          "ap": 127690,
+          "color": "gray",
+          "period": 0.294780
+        }
+      ]      
+    },
+    {
+      "name":"Saturn",
+      "ap": 1513325783,
+      "r": 58232,
+      "color": "#FD1",
+      "rings": true,
+      "satellites": [
+        {
+          "name": "Mimas",
+          "r": "198",
+          "ap": "185539",
+          "period": "0.942422",
+          "color": "gray"
+        },
+        {
+          "name": "Enceladus",
+          "r": "252",
+          "ap": "237948",
+          "period": "1.370218",
+          "color": "#77F"
+        },
+        {
+          "name": "Tethys",
+          "r": "531",
+          "ap": "294619",
+          "period": "1.887802",
+          "color": "gray"
+        },
+        {
+          "name": "Dione",
+          "r": "561.5",
+          "ap": "377396",
+          "period": "2.736915",
+          "color": "gray"
+        },
+        {
+          "name": "Rhea",
+          "r": "763.5",
+          "ap": "527108",
+          "period": "4.518212",
+          "color": "gray"
+        },
+        {
+          "name": "Titan",
+          "r": "2575",
+          "ap": "1221870",
+          "period": "15.94542",
+          "color": "#d90"
+        },
+        {
+          "name": "Iapetus",
+          "r": "735",
+          "ap": "3560820",
+          "period": "79.3215",
+          "color": "#333"
+        }
+      ]
+    },
+    {
+      "name":"Uranus",
+      "ap": 3006224700,
+      "r": 25362,
+      "color": "#0FF",
+      "satellites": [
+        {
+          "name": "Titania",
+          "diameter": "1576.8",
+          "r": "788.4",
+          "ap": "435910",
+          "period": "8.705872",
+          "color": "gray"
+        },
+        {
+          "name": "Oberon",
+          "diameter": "1522.8",
+          "r": "761.4",
+          "ap": "583520",
+          "period": "13.463239",
+          "color": "gray"
+        },
+        {
+          "name": "Ariel",
+          "diameter": "1157.8",
+          "r": "578.9",
+          "ap": "191020",
+          "period": "2.520379",
+          "color": "gray"
+        },
+        {
+          "name": "Umbriel",
+          "diameter": "1169.4",
+          "r": "584.7",
+          "ap": "266300",
+          "period": "4.144177",
+          "color": "#444"
+        },
+        {
+          "name": "Miranda",
+          "diameter": "471.6",
+          "r": "235.8",
+          "ap": "129390",
+          "period": "1.413479",
+          "color": "#999"
+        },
+        {
+          "name": "Puck",
+          "diameter": "162",
+          "r": "81",
+          "ap": "86010",
+          "period": "0.761833",
+          "color": "gray"
+        }
+      ]        
+    },
+    {
+      "name":"Neptune",
+      "ap": 4537580900,
+      "r": 24622,
+      "color": "#00F",
+      "satellites": [
+        {
+          "name": "Naiad",
+          "diameter": "66",
+          "r": "33",
+          "ap": "48227",
+          "period": "0.294"
+        },
+        {
+          "name": "Thalassa",
+          "diameter": "82",
+          "r": "41",
+          "ap": "50074",
+          "period": "0.311"
+        },
+        {
+          "name": "Despina",
+          "diameter": "150",
+          "r": "75",
+          "ap": "52526",
+          "period": "0.335"
+        },
+        {
+          "name": "Galatea",
+          "diameter": "176",
+          "r": "88",
+          "ap": "61953",
+          "period": "0.429"
+        },
+        {
+          "name": "Larissa",
+          "diameter": "194",
+          "r": "97",
+          "ap": "73548",
+          "period": "0.555"
+        },
+        {
+          "name": "Proteus",
+          "diameter": "420",
+          "r": "210",
+          "ap": "117646",
+          "period": "1.122"
+        },
+        {
+          "name": "Triton",
+          "diameter": "2705.2",
+          "r": "1352.6",
+          "ap": "354759",
+          "period": "−5.877"
+        }
+      ]
+    }
+  ]
+};
